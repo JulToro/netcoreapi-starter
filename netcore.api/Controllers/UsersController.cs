6a39219d7ef819267e1ce9058563714bdf8609ae
Andrés Londoño <andreslon@outlook.com>
2019-02-14 13:36:19 -0500

    Implementacion de configuracion de entornos y swagger

---
+++ netcore.api/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace netcore.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        /// <summary>
+        /// HOLI USERS
+        /// </summary>
+        public IConfiguration Configuration { get; set; }
+        public UsersController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        /// <summary>
+        /// ESTO ES UN METODO GET PARA OBTENER TODITO
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2", Configuration["Entorno"] };
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Holi post
+        /// </summary>
+        /// <param name="value"></param>
+        // POST: api/Users
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
